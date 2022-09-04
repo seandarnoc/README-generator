b42b1ea05c137eb3bef9a8ce50263c9984fede45
Sean Conrad <sean.w.conrad@gmail.com>
2022-09-04 08:10:29 -0400

    wrote the questions in index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,86 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generatMarkdown = require('./Develop/utils/generateMarkdown');
+const { type } = require('os');
+
+
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+    {
+        type: 'input',
+        name: 'project_title',
+        message: "What is the title/name of your project?"
+    },
+    
+    {
+        type: 'input',
+        name: 'description_of_project',
+        message: "Describe your project here:"
+    },
+
+    {
+        type: 'input',
+        name: 'installation_instructions',
+        message: "how do you run the project? (include any dependencies)"
+    },
+
+    {
+        type: 'input',
+        name: 'usage_information',
+        message: "How do you use this project? (provide instructions)"
+    },
+
+    {
+        type: 'input',
+        name: 'contribution_guidelines',
+        message: "Explain here what is required if another developer wishes to contribute:"
+    },
+
+    {
+        type: 'input',
+        name: 'test.instructions',
+        message: "provide code examples to help another contributor:"
+    },
+
+    {
+        type: 'list',
+        name: 'project_license',
+        message: "Choose your license:",
+        choices: ['Boost', 'Eclipse', 'MIT', 'Apache'],
+        filter(val) {
+            return val;
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'github username',
+        message: "What is your github username?"
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let readme = generatMarkdown(data);
+    fs.appendFile(fileName, readme)
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .pr
+}
 
 // Function call to initialize app
 init();
